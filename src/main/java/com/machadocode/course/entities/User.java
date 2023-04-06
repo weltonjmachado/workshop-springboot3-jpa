ad6336e981f90a2c63e35f3f91d5de8477ffa958
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-06 15:57:39 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/machadocode/course/entities/User.java
+++ src/main/java/com/machadocode/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.machadocode.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name  = "tb_user")
@@ -21,6 +24,8 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -33,6 +38,8 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -63,6 +70,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -78,6 +89,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 }
