88b87ad1cfdffa32848cc18817f901a0885c41ec
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-10 17:31:35 -0300

    Product

---
+++ src/main/java/com/machadocode/course/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.machadocode.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.machadocode.course.entities.Product;
+import com.machadocode.course.repositories.ProductRepository;
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
