4af2d083899922c2ccf60b6736655bdf9481739e
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-12 18:32:42 -0300

    Payment, one-to-one association

--- src/main/java/com/machadocode/course/config/TestConfig.java
+++ src/main/java/com/machadocode/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.machadocode.course.entities.Category;
 import com.machadocode.course.entities.Order;
 import com.machadocode.course.entities.OrderItem;
+import com.machadocode.course.entities.Payment;
 import com.machadocode.course.entities.Product;
 import com.machadocode.course.entities.User;
 import com.machadocode.course.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
