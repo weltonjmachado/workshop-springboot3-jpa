48bbbd7922d55d6e01a76e273d3e948ee6aa49e0
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-13 16:24:45 -0300

    User insert

--- src/main/java/com/machadocode/course/services/UserService.java
+++ src/main/java/com/machadocode/course/services/UserService.java
@@ -21,4 +21,10 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	
 }
