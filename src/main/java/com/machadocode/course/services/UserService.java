8c039bed9743f576acedbb237a20672e2573aa52
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-17 14:50:09 -0300

    Exception handling - findById

--- src/main/java/com/machadocode/course/services/UserService.java
+++ src/main/java/com/machadocode/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.machadocode.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.machadocode.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
