56ba80850e2ddc36415fcc8f0a53049351fa2ba1
Welton Johni Machado <welton.jmachado@outlook.com>
2023-04-18 10:44:07 -0300

    Exception handling - delete

--- src/main/java/com/machadocode/course/services/UserService.java
+++ src/main/java/com/machadocode/course/services/UserService.java
@@ -3,8 +3,11 @@ package com.machadocode.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.machadocode.course.services.exceptions.DatabaseException;
 import com.machadocode.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.machadocode.course.entities.User;
@@ -29,7 +32,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
